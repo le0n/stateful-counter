a0c268f719902227e7352430264295275922024b
Leon Zheng <leon.zheng@hotmail.com>
2022-05-24 19:44:31 +1000

    deleted a bunch of useless

---
+++ src/initFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDekgVKcPCmGowCZb_zp7OXdjJyXVM1Z_U",
+  authDomain: "stateful-counter-1f12c.firebaseapp.com",
+  projectId: "stateful-counter-1f12c",
+  storageBucket: "stateful-counter-1f12c.appspot.com",
+  messagingSenderId: "563241930461",
+  appId: "1:563241930461:web:5fd6796db8caae2d0e5534"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
